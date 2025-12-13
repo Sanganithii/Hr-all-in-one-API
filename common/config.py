5ae3400fe63aabe767aeabcac0ab80a2b64775ba
Sanganithi <sanganithi@andiran.in>
2025-12-13 16:05:10 +0530

    Fix structure: track app folder and remove duplicates

--- common/config.py
+++
@@ -1,7 +0,0 @@
-class APIConfig:
-
-    class AuthCredential:
-        
-        SECRET_KEY = "Mic5ua58q-UHAXj1k-tvAWasSjv_Ceyv1aRluQJsDHo"
-        ALGORITHM = "HS256"
-        ACCESS_TOKEN_EXPIRE_MINUTES = 30
